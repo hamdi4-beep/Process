331e60963e54596bc4ee106c8e7e7a1f7bfe811f
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2022-11-05 17:04:29 -0700

    added new files

--- js/script.js
+++ js/script.js
@@ -1,58 +1,34 @@
-const main = document.querySelector('main')
-const userImg = document.querySelector('.user-img')
-const input = document.querySelector('input')
 const btn = document.querySelector('button')
+const input = document.querySelector('input')
+const section = document.querySelector('section')
+const imgs = document.querySelectorAll('.img')
 
-userImg.addEventListener('click', e => {
-    const img = userImg.querySelector('img')
-    showFullScreen(img)
+let currentElem
+
+btn.addEventListener('pointerdown', e => section.classList.toggle('align'))
+
+input.addEventListener('change', ({ currentTarget }) => {
+    const value = currentTarget.value
+
+    if (currentElem && value) {
+        currentElem.style.backgroundImage = `url(${value})`
+        currentTarget.value = ''
+    }
 })
 
-input.addEventListener('change', ({ currentTarget }) => addImg(currentTarget.value))
-input.addEventListener('blur', ({ currentTarget }) => currentTarget.value = '')
-btn.addEventListener('click', e => uploadImg(addImg))
+for (const img of imgs) {
+    img.addEventListener('pointerdown', ({ currentTarget }) => {
+        currentTarget.classList.add('target')
+        selectImg(currentTarget, target => currentElem = target)
+    })
+}
 
-function uploadImg(callback) {
-    const input = document.createElement('input')
-
-    input.type = 'file'
-    input.click()
-
-    input.onchange = ({ currentTarget: { files } }) => {
-        if (files[0]) {
-            const file = files[0]
-            const type = file.type
-
-            if (type.split('/')[0] !== 'image') return
-
-            callback && callback(URL.createObjectURL(file))
+function selectImg(elem, callback) {
+    imgs.forEach(img => {
+        if (elem == img) {
+            callback(elem)
+        } else {
+            img.classList.remove('target')
         }
-    }
-
-    input.remove()
-}
-
-function addImg(url) {
-    const imgsContainer = document.querySelector('.imgs-container')
-    const div = document.createElement('div')
-    const img = new Image
-
-    div.onclick = e => showFullScreen(img)
-
-    div.className = 'img-wrapper'
-    img.src = url
-
-    div.append(img)
-    imgsContainer.append(div)
-}
-
-function showFullScreen(img) {
-    const layer = document.createElement('div')
-    const cloneImg = img.cloneNode()
-
-    layer.onclick = e => layer.remove()
-    layer.className = 'layer'
-
-    main.append(layer)
-    layer.append(cloneImg)
+    })
 }
