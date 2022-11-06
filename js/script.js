9ca4a6e0b698a95d322d7da7cec2afeacd08a05d
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2022-11-06 10:13:21 -0800

    created mirror effect

--- js/script.js
+++ js/script.js
@@ -1,33 +1,43 @@
-const btn = document.querySelector('button')
-const input = document.querySelector('input')
-const section = document.querySelector('section')
+const mirrored = document.querySelector('.mirror')
 const imgs = document.querySelectorAll('.img')
+const input = document.querySelector('input')
 
-let currentElem
-
-btn.addEventListener('pointerdown', e => section.classList.toggle('align'))
+let currentElem, currentMatched
 
 input.addEventListener('change', ({ currentTarget }) => {
     const value = currentTarget.value
 
     if (currentElem && value) {
-        currentElem.style.backgroundImage = `url(${value})`
+        const img = currentElem.querySelector('img')
+        const matchedImg = currentMatched.querySelector('img')
+
+        img.src = value
+        matchedImg.src = img.src
         currentTarget.value = ''
     }
 })
 
 for (const img of imgs) {
     img.addEventListener('pointerdown', ({ currentTarget }) => {
-        currentTarget.classList.add('target')
-        selectImg(currentTarget, target => currentElem = target)
+        selectImg(currentTarget, (elem, i) => {
+            if (elem.classList.contains('target')) currentElem = elem
+        })
     })
 }
 
 function selectImg(elem, callback) {
-    imgs.forEach(img => {
+    imgs.forEach((img, i) => {
+        const matched = mirrored.querySelectorAll('.img')[i]
+
         if (elem == img) {
-            callback(elem)
+            elem.classList.toggle('target')
+            matched && matched.classList.toggle('matched')
+
+            currentMatched = matched
+
+            callback(elem, i)
         } else {
+            matched && matched.classList.remove('matched')
             img.classList.remove('target')
         }
     })
