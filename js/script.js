aed85c47a71244d5d8af3d28fedc8b620f7834b0
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2022-12-09 10:33:56 -0800

    fixed a bug that shows arguments as undefined

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 import _ from './process.js'
 
-_('hamdi4-beep').invoke(function(){})
-
-_().invoke(username => {
-    const _self = _()
-
-    _self.log({
-        username
-    })
+_(1).invoke(function(a, b, c) {
+    console.log(a, b, c)
 })
