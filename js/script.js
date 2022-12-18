12f408edd5e28bf708480e3e9bae7f2e095c6ac0
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2022-12-18 10:38:37 -0800

    added usage examples

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
 import _ from './process.js'
 
-_('main')
-.invoke(element => {
-    element.addEventListener('pointerdown', ({ target }) => {
+
+// looks up DOM child with that string
+
+_('.theme-list')
+
+
+// parameter refers to the element in the DOM (theme-list)
+
+.invoke(container => {
+    container.addEventListener('pointerdown', ({ target }) => {
+        const _h2 = _('h2') // methods called through this variable will reflect on the first argument (h2 element)
         const _child = _(target)
 
-        _child.addCSS({
-            'background-color': 'black',
-            color: 'white',
-            toggle: true
+        _h2.invoke(function(element) {
+
+
+            // fetches CSS properties of the element that was passed to child variable
+
+            const color = _child.getCSS('background-color')
+
+
+            // the this keyword here refers to the object that owns "utility methods"
+            // methods reflect changes on the element that was passed to the _ function
+
+            this
+            .addCSS({
+                position: 'relative',
+                left: '-50%',
+                transform: 'rotate(360deg) scale(.5)',
+                toggle: true // property that "toggles" the CSS properties if present on the object
+            })
+            .addCSS({
+                'background-color': color,
+                color: 'white'
+            })
+
+            element.onclick = e => {
+                this.addCSS({
+                    opacity: 0,
+                    toggle: true
+                })
+            }
         })
     })
 })
